70fc103444820b8836ae3b5c3af6df9cfcfe793f
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-02-04 14:57:24 +0200

    db driver

---
+++ includes/db.inc.php
@@ -0,0 +1,21 @@
+<?php
+$host = "db";
+$dbname = "santaroso";
+$dbusername = "root"; // root
+$dbpassword = "root"; // empty
+
+//$config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/configs.ini', true)['db'];
+//
+//$host = $config['host'];
+//$dbname = $config['dbname'];
+//$dbusername = $config['username']; // root
+//$dbpassword = $config['password']; // empty
+
+$dsn = "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4";
+
+try{
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connection failed: " . $e->getMessage();
+}
